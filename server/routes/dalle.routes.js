7eb896c6ef225c7b18985026e93f1833828e793c
Humoyun <you@example.com>
2024-01-27 16:16:07 +0500

    3D

---
+++ server/routes/dalle.routes.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import * as dotenv from 'dotenv'
+import { Configuration, OpenAIApi } from 'openai'
+
+
+dotenv.config();
+
+const router = express.Router();
+
+const confing = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY
+})
+
+const openai = new OpenAIApi(confing);
+
+
+
+router.route('/').get((req, res) => {
+    res.status(200).json({ messege: "Hello from DALL.E ROUTES" })
+})
+
+
+router.route('/').post(async (req, res) => {
+    try {
+        const response = await openai.createImage({
+            prompt, 
+            n: 1, 
+            size: '1024x1024',
+            response_format: 'b64_json'
+        });
+
+
+
+        const image = response.data .data [0].b64_json;
+
+        res.status(200).json({ photo: image })
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ messege: "Something went wrong" })
+    }
+})
+
+export default router;
